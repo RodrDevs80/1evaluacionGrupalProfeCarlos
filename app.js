e1f9080cdb0f3401d15f3b0f602b95fc3b87bfa0
RodrDevs80 <rodr26707@gmail.com>
2024-05-06 08:40:00 -0300

    modificacion La Perdedora-revision de menu punto 1.Carga y Clasificacion y se agregaron opciones: mostrar ingresos e informe final al salir del programa

--- app.js
+++ app.js
@@ -30,21 +30,23 @@ const silos = [{
     nombre: 'Silo1',
     cantidadDeMaiz: 0,
     tipoDeMaiz: 'alta calidad',
-    amacenamientoMaximo: 4000
+    almacenamientoMaximo: 4000
 },
 {
     nombre: 'Silo2',
     cantidadDeMaiz: 0,
     tipoDeMaiz: 'presenta objetos extraños',
-    amacenamientoMaximo: 3000
+    almacenamientoMaximo: 3000
 },
 {
     nombre: 'Silo3',
     cantidadDeMaiz: 0,
     tipoDeMaiz: 'presenta humedad',
-    amacenamientoMaximo: 3000
+    almacenamientoMaximo: 3000
 },
 ];
+const ingresos = [];
+let idMaiz = 0;
 //variables-objetos
 const maiz = {
     procedencia: '',
@@ -75,26 +77,37 @@ function validarEntradaDeDatos(origen, cantidad, estado) {
 
 function mostarStock(array) {
     for (let i = 0; i < array.length; i++) {
-        alert(`El ${array[i].nombre}, tiene ${array[i].cantidadDeMaiz} toneladas, Calidad: ${array[i].tipoDeMaiz},Capacidad libre:${array[i].amacenamientoMaximo} Toneladas 🚚`);
+        alert(`El ${array[i].nombre}, tiene ${array[i].cantidadDeMaiz} toneladas, Calidad: ${array[i].tipoDeMaiz},Capacidad libre:${array[i].almacenamientoMaximo - array[i].cantidadDeMaiz} Toneladas 🚚`);
     }
 }
-
-function ControlarIngresoAlSilo(array, index) {
-    if (array[index].cantidadDeMaiz >= 0 && array[index].cantidadDeMaiz < 3000) {
-        array[index].cantidadDeMaiz += maiz.cantidad;
-        array[index].amacenamientoMaximo -= array[index].cantidadDeMaiz;
+//⚠❓
+function ControlarIngresoAlSilo(array, index, cant) {
+    if (array[index].cantidadDeMaiz >= 0 && array[index].cantidadDeMaiz <= array[index].almacenamientoMaximo) {
+        array[index].cantidadDeMaiz += cant;
+        alert('Ingreso realizado con éxito!💾✔')
     } else {
         alert('Supera la cantidad de almacenaje del silo!⚠');
     }
+
 }
 
+function informeGeneralSilos(array) {
+    const informe = `\n**INFORME GENERAL DE LOS INGRESOS DE MAÍZ 🖨***\n${array[0].nombre},contiene:🌽${array[0].cantidadDeMaiz} TN 🚍. Espacio disponible: ${array[0].almacenamientoMaximo - array[0].cantidadDeMaiz}TN 🚍. Calidad: ${array[0].tipoDeMaiz} 🥇 \n ${array[1].nombre},contiene:🌽${array[1].cantidadDeMaiz} TN 🚍. Espacio disponible: ${array[1].almacenamientoMaximo - array[1].cantidadDeMaiz}TN 🚍. Calidad: ${array[1].tipoDeMaiz} 🧩\n${array[2].nombre},contiene:🌽${array[2].cantidadDeMaiz} TN 🚍. Espacio disponible: ${array[2].almacenamientoMaximo - array[2].cantidadDeMaiz}TN 🚍. Calidad: ${array[2].tipoDeMaiz} 💧 `;
+    return informe;
+}
+
+function mostarIngresosDeMaiz(array) {
+    for (let i = 0; i < array.length; i++) {
+        alert(`\n N° de Ingreso: ${i + 1}\n Procedencia: ${array[i].procedencia}🚚\n Cantidad: ${array[i].cantidad} TN ⚖\n Tipo: ${array[i].estado} 📝`);
+    }
+}
 
 function ingresarAndClasificarMaiz(array, maiz) {
 
     do {
         const maizIngresadoOrigen = prompt('Ingrese la procedencia del maíz:');
         const maizIngresadoCantidad = prompt('Ingrese la cantidad de maíz en toneladas:');
-        const maizIngresadoEstado = prompt(`\n**Calidad del Maiz 🌽 **\n1️⃣. Alta calidad\n2️⃣.Presencia de cuerpos extraños \n3️⃣. Presencia de Humedad\n\nElija una opción:`);
+        const maizIngresadoEstado = prompt(`\n**Calidad del Maiz 🌽 **\n1️⃣. Alta calidad🥇\n2️⃣. Presencia de cuerpos extraños🧩\n3️⃣. Presencia de Humedad💧\n\nElija una opción:`);
         condicion = validarEntradaDeDatos(maizIngresadoOrigen, maizIngresadoCantidad, maizIngresadoEstado);
         if (condicion.includes('ERROR')) {
             alert(condicion);
@@ -103,15 +116,24 @@ function ingresarAndClasificarMaiz(array, maiz) {
     maiz.procedencia = condicion[0];
     maiz.cantidad = condicion[1];
     maiz.estado = condicion[2];
+
+    ingresos.push({
+        id: idMaiz,
+        procedencia: condicion[0],
+        cantidad: condicion[1],
+        estado: condicion[2]
+    });
+    idMaiz++;
+    console.log(ingresos);
     switch (maiz.estado) {
         case 1:
-            array[0].amacenamientoMaximo < maiz.cantidad ? alert('Supera la capacidad maxima del silo,introduzca una cantidad valida ⚠') : ControlarIngresoAlSilo(array, 0);
+            array[0].almacenamientoMaximo < maiz.cantidad || array[0].almacenamientoMaximo - array[0].cantidadDeMaiz < maiz.cantidad ? alert('Supera la capacidad maxima del silo,introduzca una cantidad valida ⚠') : ControlarIngresoAlSilo(array, 0, maiz.cantidad);
             break;
         case 2:
-            array[1].amacenamientoMaximo < maiz.cantidad ? alert('Supera la capacidad maxima del silo,introduzca una cantidad valida ⚠') : ControlarIngresoAlSilo(array, 1);
+            array[1].almacenamientoMaximo < maiz.cantidad || array[1].almacenamientoMaximo - array[1].cantidadDeMaiz < maiz.cantidad ? alert('Supera la capacidad maxima del silo,introduzca una cantidad valida ⚠') : ControlarIngresoAlSilo(array, 1, maiz.cantidad);
             break;
         case 3:
-            array[2].amacenamientoMaximo < maiz.cantidad ? alert('Supera la capacidad maxima del silo,introduzca una cantidad valida ⚠') : ControlarIngresoAlSilo(array, 2);
+            array[2].almacenamientoMaximo < maiz.cantidad || array[2].almacenamientoMaximo - array[2].cantidadDeMaiz < maiz.cantidad ? alert('Supera la capacidad maxima del silo,introduzca una cantidad valida ⚠') : ControlarIngresoAlSilo(array, 2, maiz.cantidad);
             break;
         default:
             console.log('opción no valida!');
@@ -123,7 +145,7 @@ function ingresarAndClasificarMaiz(array, maiz) {
 
 //main->Menu principal
 do {
-    opcion = prompt(`\n**Empresa La Perdedora 🕋**\n**Sistema de Ingreso y Clasificación de Maiz 🌽 **\n1️⃣. Ingresar Maiz y Clasificar Maiz\n2️⃣.Mostrar stock actual \n3️⃣. Salir\n\nElija una opción:`);
+    opcion = prompt(`\n**Empresa La Perdedora 🕋**\n**Sistema de Ingreso y Clasificación de Maiz 🌽 **\n1️⃣. Ingresar Maiz y Clasificar Maiz🏷\n2️⃣. Mostrar stock actual👁\n3️⃣. Mostrar Ingresos de Maiz ⏬🗃\n4️⃣. Salir🚪\n\nElija una opción:`);
     switch (opcion) {
         case '1':
             ingresarAndClasificarMaiz(silos, maiz);
@@ -132,10 +154,14 @@ do {
             mostarStock(silos);
             break;
         case '3':
+            ingresos.length == 0 ? alert('No se realizo ningún ingreso!🚨') : mostarIngresosDeMaiz(ingresos);
+            break;
+        case '4':
+            alert(informeGeneralSilos(silos));
             break;
         default:
-            alert('\nIngreso una opción no valida!\nLas opciones validas están en el rango de 1 a 3 ❌')
+            alert('\nIngreso una opción no valida!\nLas opciones validas están en el rango de 1 a 4 ❌')
             break;
     }
 
-} while (opcion != 3);
+} while (opcion != 4);
